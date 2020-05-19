6c7f24bbc59c1debf6e73bd043486870723b30c8
jor504 <jor504@gmail.com>
2020-05-18 21:33:05 -0600

    First Commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas en Cine'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search), 
+            onPressed: (){
+
+            }
+            )
+        ],
+      ),
+
+      //Widget para desplegar informacion donde no esta el notch SafeArea
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas()
+          ],
+        ),
+      )
+      
+      
+      
+      
+    );
+  }
+
+  Widget _swiperTarjetas() {
+
+    return  Container(
+      padding: EdgeInsets.only(top:10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+          itemBuilder: (BuildContext context,int index){
+            return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+          },
+          itemCount: 3,
+          itemWidth: 200.0,
+          layout: SwiperLayout.STACK,
+          //pagination: new SwiperPagination(),
+          //control: new SwiperControl(),
+        ),
+    );
+
+
+  }
+}
