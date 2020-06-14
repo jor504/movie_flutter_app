e9d75b210855b818898af627a82321ab7a6947bc
jor504 <jor504@gmail.com>
2020-06-13 23:09:55 -0600

    Adding Cast info in the detail page

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,3 +1,4 @@
+import 'package:peliculas/src/models/actores_model.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -76,6 +77,22 @@ class PeliculasProvider{
     _cargando =false;
     return resp;
 
+  }
+
+  Future<List<Actor>> getCast(String peliId) async {
+
+    final url = Uri.https(_url, '3/movie/$peliId/credits',{
+      'api_key' : _apikey,
+     'language' :_language,
+    });
+  
+    final resp = await http.get(url);
+
+    final decodedData = json.decode(resp.body);
+  
+    final cast = new Cast.fromJsonList(decodedData['cast']);
+  
+    return  cast.actores;
   }
 
 
